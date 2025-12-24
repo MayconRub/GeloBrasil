fd92c9d4355a1b13ecfe43896bcbff964f566a72
MayconRub <mayconrubemx@gmail.com>
2025-12-24 10:38:31 -0300

    Update supabaseClient.ts
    testando supabase

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://ghyodlccgtvkcmiryldb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdoeW9kbGNjZ3R2a2NtaXJ5bGRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyNzQyNjUsImV4cCI6MjA4MTg1MDI2NX0.YuTZV5MHavakx6vWBcdxSoveelfv4kkyf5JfF1n-uxc';
+const VITE_SUPABASE_URL = 'https://ghyodlccgtvkcmiryldb.supabase.co';
+const VITE_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdoeW9kbGNjZ3R2a2NtaXJ5bGRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyNzQyNjUsImV4cCI6MjA4MTg1MDI2NX0.YuTZV5MHavakx6vWBcdxSoveelfv4kkyf5JfF1n-uxc';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
